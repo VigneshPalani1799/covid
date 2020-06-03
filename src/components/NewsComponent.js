44963e80338da27c1c47773a8bba87e4d25d66af
vigneshpalani003 <vigneshpalani003@gmail.com>
2020-06-03 23:34:58 +0530

    News Component

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -1,7 +1,45 @@
 import React, { Component } from 'react';
-
+import Fade from 'react-reveal/Fade';
 import { Card } from 'reactstrap';
 import axios from 'axios';
+
+
+function RenderNews({newsData}){
+    if(newsData!==null){
+        return(
+            newsData.map((news) => {
+                return(
+                    <div className="container mt-2 mb-1" >
+                        <Fade left>
+                            <a href={news.url} target="_blank" style={{textDecoration:"none"}}>
+                                <Card>
+                                    <div className="row">
+                                        <div className="col-6 col-sm-4">
+                                            <img src={news.urlToImage} 
+                                                className="img-fluid img-thumbnail rounded" 
+                                                alt="Error fetching image"
+                                                href={news.url}
+                                            />
+                                            
+                                        </div>
+                                        <div className="col-6 col-sm-8">
+                                            <h5>{news.title}</h5>
+                                            <p className="d-none d-sm-block">{news.content}</p>
+                                            <small className="text-muted">{new Date(news.publishedAt).toString()}</small>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </a>
+                        </Fade>
+                    </div>
+                );
+            })
+        );
+    }
+    else
+        return(<div></div>);
+
+}
 
 
 class News extends Component{
@@ -11,20 +49,24 @@ class News extends Component{
 
         this.state={
             newsData:[],
-            url:'http://newsapi.org/v2/everything?q=covid19&from=2020-05-02&sortBy=publishedAt&apiKey=f9a846f8e8df4dc1904d9d736262a62c'
+            url:'http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=f9a846f8e8df4dc1904d9d736262a62c'
         };
     }
 
     componentDidMount(){
         axios.get(this.state.url)
-        .then(result => console.log(result))
+        .then((result) => {
+            console.log(result);
+            this.setState({newsData:result.data.articles});
+        })
         .catch(error => console.log(error));
     }
 
     render(){
         return(
-            <div>
-                <Card></Card>
+            <div className="container">
+                <h3 className="text-dark m-3">News Headlines</h3>
+                <RenderNews newsData={this.state.newsData}/>
             </div>
         );
     }
